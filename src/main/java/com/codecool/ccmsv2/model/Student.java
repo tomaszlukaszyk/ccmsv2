508493e184ee0fc3314ff3bb89b0a0e036d3d39b
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-07-23 11:42:26 +0200

    Add Student class

---
+++ src/main/java/com/codecool/ccmsv2/model/Student.java
@@ -0,0 +1,8 @@
+package com.codecool.ccmsv2.model;
+
+public class Student extends User {
+
+    public Student(String name, String email, String password) {
+        super(name, email, password);
+    }
+}
