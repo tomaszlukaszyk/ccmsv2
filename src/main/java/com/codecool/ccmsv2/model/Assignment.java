67e46376e6a0ac33d06f73b18a85b5d054146038
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-07-23 12:53:43 +0200

    Add Assignment class

---
+++ src/main/java/com/codecool/ccmsv2/model/Assignment.java
@@ -0,0 +1,51 @@
+package com.codecool.ccmsv2.model;
+
+public class Assignment {
+
+    private String name;
+    private String description;
+    private String submissionLink;
+    private String grade;
+
+    public Assignment(String name, String description, String submissionLink, String grade) {
+        this.name = name;
+        this.description = description;
+        this.submissionLink = submissionLink;
+        this.grade = grade;
+    }
+
+    public Assignment(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.submissionLink = "";
+        this.grade = "0";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSubmissionLink() {
+        return submissionLink;
+    }
+
+    public void setSubmissionLink(String submissionLink) {
+        this.submissionLink = submissionLink;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String toString() {
+        return String.format("%s\n%s", this.name, this.description);
+    }
+}
