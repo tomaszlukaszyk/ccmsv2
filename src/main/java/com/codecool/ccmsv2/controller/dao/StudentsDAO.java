7b3d3b9a7bd317cc15c6ac6aa55e616fd8810d21
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-07-23 23:16:11 +0200

    Implemented XMLStudentsDao methods.

--- src/main/java/com/codecool/ccmsv2/controller/dao/StudentsDAO.java
+++ src/main/java/com/codecool/ccmsv2/controller/dao/StudentsDAO.java
@@ -7,9 +7,8 @@ import java.util.List;
 
 public interface StudentsDAO {
     List<String> readStudentsEmails();
-    Student readStudentByEmail();
+    Student readStudentByEmail(String email);
     List<Student> readStudents();
     void writeStudents(List<Student> students);
     List<Assignment> readStudentAssignments(String email);
-    void writeStudentAssignments(List<Assignment> assignments, String email);
 }
