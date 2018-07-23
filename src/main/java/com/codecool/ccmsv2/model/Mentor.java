2c61c81f6c35c765554ea1faf4326fe0949b83ff
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-07-23 11:42:52 +0200

    Add Mentor class

---
+++ src/main/java/com/codecool/ccmsv2/model/Mentor.java
@@ -0,0 +1,8 @@
+package com.codecool.ccmsv2.model;
+
+public class Mentor extends User {
+
+    public Mentor(String name, String email, String password){
+        super(name, email, password);
+    }
+}
