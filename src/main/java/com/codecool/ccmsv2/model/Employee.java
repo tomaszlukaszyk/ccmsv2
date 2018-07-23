a9227de8c23b98e0e923ee36944f9979db0080ce
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-07-23 11:43:22 +0200

    Add Employee class

---
+++ src/main/java/com/codecool/ccmsv2/model/Employee.java
@@ -0,0 +1,8 @@
+package com.codecool.ccmsv2.model;
+
+public class Employee extends User {
+
+    public Employee(String name, String email, String password) {
+        super(name, email, password);
+    }
+}
