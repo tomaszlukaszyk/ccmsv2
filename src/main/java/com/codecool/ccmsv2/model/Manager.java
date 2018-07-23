8f26e91165479cc6a81702da5a7d57c67b9accf2
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-07-23 11:43:34 +0200

    Add Manager class

---
+++ src/main/java/com/codecool/ccmsv2/model/Manager.java
@@ -0,0 +1,8 @@
+package com.codecool.ccmsv2.model;
+
+public class Manager extends User {
+
+    public Manager(String name, String email, String password) {
+        super(name, email, password);
+    }
+}
