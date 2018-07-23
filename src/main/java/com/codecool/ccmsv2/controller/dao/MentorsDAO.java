d1a21a0353de22f409f2642671595fd15ba84432
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-07-23 14:59:28 +0200

    Add MentorsDAO interface

---
+++ src/main/java/com/codecool/ccmsv2/controller/dao/MentorsDAO.java
@@ -0,0 +1,12 @@
+package com.codecool.ccmsv2.controller.dao;
+
+import com.codecool.ccmsv2.model.Mentor;
+
+import java.util.List;
+
+public interface MentorsDAO {
+    List<String> readMentrosEmails();
+    Mentor readMentorByEmail(String email);
+    List<Mentor> readMentors();
+    void writeMentors(List<Mentor> mentors);
+}
