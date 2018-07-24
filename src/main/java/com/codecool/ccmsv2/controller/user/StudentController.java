450c80a0f81c1034eb19d4445d03d3756366b1a0
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-07-24 13:21:47 +0200

    Implement all SController methods

--- src/main/java/com/codecool/ccmsv2/controller/user/StudentController.java
+++ src/main/java/com/codecool/ccmsv2/controller/user/StudentController.java
@@ -15,18 +15,32 @@ public class StudentController extends UserController {
     }
 
     private void showAssignmentDescription(){
-        String name = getView().getString();
-        System.out.println(new CSVAssignmentsDAO().getAssignmentDescriptionByName(name));
+        String name = getView().getInputString("Name of assignment?");
+        System.out.println(name +"\n" + new CSVAssignmentsDAO().getAssignmentDescriptionByName(name));
     }
 
     private void submitAssignment(){
-        String name = getView().getString();
+        String name = getView().getInputString("Name of assignment?");
         XMLStudentsDAO xmlStudentsDAO = new XMLStudentsDAO();
         List<Assignment> assignmentList = xmlStudentsDAO.readStudentAssignments(getUser().getEmail());
         Assignment assignmentToSubmit = findAssignment(name, assignmentList);
-        String submissionLink = getView().getString();
+        String submissionLink = getView().getInputString("Linkt to assignment?");
         assignmentToSubmit.setSubmissionLink(submissionLink);
         xmlStudentsDAO.writeStudents(xmlStudentsDAO.readStudents(), assignmentList);
+    }
+
+    private void showGrades(){
+        List<Assignment> assignmentList = new XMLStudentsDAO().readStudentAssignments(getUser().getEmail());
+        for (Assignment assignment : assignmentList){
+            System.out.println(assignment.getName() +":  " + assignment.getGrade());
+        }
+    }
+
+    private void showAssignments(){
+        List<Assignment> assignmentList = new XMLStudentsDAO().readStudentAssignments(getUser().getEmail());
+        for (Assignment assignment : assignmentList){
+            System.out.println(assignment);
+        }
     }
 
     private Assignment findAssignment(String name, List<Assignment> assignments){
