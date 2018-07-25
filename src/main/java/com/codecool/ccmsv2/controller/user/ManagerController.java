ff69f0b2b2680a58dd9a2e572fe45daca36272ac
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-07-25 09:52:44 +0200

    Implement editMentor method

--- src/main/java/com/codecool/ccmsv2/controller/user/ManagerController.java
+++ src/main/java/com/codecool/ccmsv2/controller/user/ManagerController.java
@@ -58,7 +58,7 @@ public class ManagerController extends UserController {
                     addMentor();
                     break;
                 case 4:
-                    // TODO: edit mentor
+                    editMentor();
                     break;
                 case 5:
                     // TODO: remove mentor
@@ -82,8 +82,8 @@ public class ManagerController extends UserController {
 
     private void showUsers(List<? extends User> users) {
 
-        for (User user: users) {
-            getView().print("\n" + user);
+        for (int i=0; i<users.size(); i++) {
+            getView().print(String.format("\n%d. %s", i + 1, users.get(i)));
         }
     }
 
@@ -113,5 +113,47 @@ public class ManagerController extends UserController {
         Mentor mentor = new Mentor(basicUserData[0], basicUserData[1], basicUserData[2]);
         mentors.add(mentor);
         mentorsDAO.writeMentors(mentors);
+    }
+
+    private void editMentor() {
+
+        List<Mentor> mentors = mentorsDAO.readMentors();
+        Mentor mentor = mentors.get(chooseUser(mentors));
+        int chosenDataIndex = chooseUserData();
+
+        switch (chosenDataIndex) {
+            case 1:
+                getView().print("Old name: " + mentor.getName());
+                String name = getView().getInputString("New name: ");
+                mentor.setName(name);
+                break;
+            case 2:
+                getView().print("Old email: " + mentor.getEmail());
+                String email = getView().getInputString("New email: ");
+                mentor.setEmail(email);
+                break;
+            case 3:
+                getView().print("Old password: " + mentor.getPassword());
+                String password = getView().getInputString("New password: ");
+                mentor.setPassword(password);
+                break;
+        }
+
+    }
+
+    private int chooseUser(List<? extends User> users) {
+        showUsers(users);
+        return getView().getInputInt(1, users.size()) - 1;
+    }
+
+    private int chooseUserData() {
+        String[] data = new String[]{"name", "email", "password"};
+
+        getView().print("Choose data to edit:");
+        for (int i=0; i<data.length; i++){
+            getView().print(String.format("\n%d. %s", i + 1, data[i]));
+        }
+
+        return getView().getInputInt(1, data.length);
     }
 }
